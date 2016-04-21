ce5f509b089eb09fd84eca1398dae955c424d7da
Walter Chen <walter.chen@bhnetwork.com>
2016-04-20 19:59:39 -0700

    remove intellij files

--- src/main/java/com/bhn/adhawk/configuration/MvcConfig.java
+++ src/main/java/com/bhn/adhawk/configuration/MvcConfig.java
@@ -1,10 +1,16 @@
 package com.bhn.adhawk.configuration;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+//import org.springframework.web.multipart.commons.CommonsMultipartResolver;
  
 @EnableWebMvc
 @Configuration
@@ -34,10 +40,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         return bean;
     }
  
-    @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver getMultipartResolver() {
-        return new CommonsMultipartResolver();
-    }
+//    @Bean(name = "multipartResolver")
+//    public CommonsMultipartResolver getMultipartResolver() {
+//        return new CommonsMultipartResolver();
+//    }
  
     @Bean(name = "messageSource")
     public ReloadableResourceBundleMessageSource getMessageSource() {
